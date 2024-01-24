e8b1a48c729fb6b3ee67fb3627cdd2460cc3cfe9
ZaytsveDmitriy <d.v.z.prim@gmail.com>
2024-01-24 22:24:20 +0300

    add implementation

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,34 @@ type (
 
 type Stage func(in In) (out Out)
 
+func stageWraper(in In, done In, stage Stage) Out {
+	inputCh := make(Bi)
+	go func() {
+		defer close(inputCh)
+		for {
+			select {
+			case <-done:
+				return
+			case val, open := <-in:
+				if !open {
+					return
+				}
+				inputCh <- val
+			}
+		}
+	}()
+
+	return stage(inputCh)
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil || len(stages) < 1 {
+		return nil
+	}
+	lastOut := in
+	for _, stage := range stages {
+		lastOut = stageWraper(lastOut, done, stage)
+	}
+
+	return lastOut
 }
