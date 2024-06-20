06550b62299b3bd3b40eda5fdc20b53686d61f42
ZaytsveDmitriy <d.v.z.prim@gmail.com>
2024-06-21 00:08:41 +0300

    add implementation and tests

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,6 +2,7 @@ package hw10programoptimization
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -24,6 +25,49 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	var user User
+	const RE = "@(\\w+\\.)"
+
+	matcher, err := regexp.Compile(RE + domain)
+	if err != nil {
+		return nil, err
+	}
+
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	reader := bufio.NewReader(r)
+	stat := make(DomainStat, 100)
+
+	for i := 0; ; i++ {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, Error(err)
+		}
+		if err = json.Unmarshal(line, &user); err != nil {
+			return nil, Error(err)
+		}
+
+		matched := matcher.FindStringSubmatch(user.Email)
+
+		if len(matched) != 0 {
+			key := strings.ToLower(matched[1] + domain)
+			num := stat[key]
+			num++
+			stat[key] = num
+		}
+	}
+
+	return stat, nil
+}
+
+func Error(err error) error {
+	const message = "get users error: %w"
+	return fmt.Errorf(message, err)
+}
+
+func GetDomainStatAld(r io.Reader, domain string) (DomainStat, error) {
 	u, err := getUsers(r)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
@@ -34,41 +78,35 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	json := jsoniter.ConfigCompatibleWithStandardLibrary
-	reader := bufio.NewReader(r)
+	content, err := io.ReadAll(r)
+	if err != nil {
+		return
+	}
 
-	for i := 0; ; i++ {
-		line, _, err := reader.ReadLine()
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				return result, nil
-			}
-			return result, err
-		}
+	lines := strings.Split(string(content), "\n")
+	for i, line := range lines {
 		var user User
-		if err = json.Unmarshal(line, &user); err != nil {
-			return result, err
+		if err = json.Unmarshal([]byte(line), &user); err != nil {
+			return
 		}
 		result[i] = user
 	}
+	return
 }
 
 func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat, 100)
-
-	matcher, err := regexp.Compile("@(\\w+\\.)" + domain)
-	if err != nil {
-		return nil, err
-	}
+	result := make(DomainStat)
 
 	for _, user := range u {
-		matched := matcher.FindSubmatch([]byte(user.Email))
+		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+		if err != nil {
+			return nil, err
+		}
 
-		if len(matched) != 0 {
-			key := strings.ToLower(string(matched[1]) + domain)
-			num := result[key]
+		if matched {
+			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
 			num++
-			result[key] = num
+			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
 		}
 	}
 	return result, nil
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -54,36 +54,3 @@ func BenchmarkStat(b *testing.B) {
 		GetDomainStat(data, "biz")
 	}
 }
-
-func BenchmarkGetUser(b *testing.B) {
-	r, err := zip.OpenReader("testdata/users.dat.zip")
-	if err != nil {
-		return
-	}
-	for i := 0; i < b.N; i++ {
-
-		data, err := r.File[0].Open()
-		if err != nil {
-			return
-		}
-		getUsers(data)
-	}
-}
-
-func BenchmarkCountDomains(b *testing.B) {
-	r, err := zip.OpenReader("testdata/users.dat.zip")
-	if err != nil {
-		return
-	}
-	data, err := r.File[0].Open()
-	if err != nil {
-		return
-	}
-	users, err := getUsers(data)
-	if err != nil {
-		return
-	}
-	for i := 0; i < b.N; i++ {
-		countDomains(users, "biz")
-	}
-}
